4452c3661a80278466ba0ca3b2ee762ce310c04c
MIKKACHTHAMAS <mikkachthamas@gmail.com>
2026-01-20 22:55:51 +0530

    my first project

---
+++ web.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+import psycopg2
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+#Secure cookies
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_SAMESITE="Lax"
+)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+DATABASE_URL = os.environ.get("database\schema.sql")
+
+def get_db():
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
+
+#User Model
+class User(UserMixin):
+    def __init__(self, id, username, password_hash):
+        self.id = id
+        self.username = username
+        self.password_hash = password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT id, username, password_hash FROM users WHERE id=%s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return User(*user) if user else None
+
+# login
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT id, username, password_hash FROM users WHERE username=%s",
+            (username,)
+        )
+        user = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if user and bcrypt.check_password_hash(user[2], password):
+            login_user(User(*user))
+            return redirect(url_for("home"))
+
+        return "Invalid credentials", 401
+
+    return render_template("templates\login.html")
+
+#Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        hashed = bcrypt.generate_password_hash(password).decode("utf-8")
+
+        conn = get_db()
+        cur = conn.cursor()
+
+        try:
+            cur.execute(
+                "INSERT INTO users (username, password_hash) VALUES (%s, %s)",
+                (username, hashed)
+            )
+            conn.commit()
+        except psycopg2.errors.UniqueViolation:
+            conn.rollback()
+            return "Username already exists"
+        finally:
+            cur.close()
+            conn.close()
+
+        return redirect(url_for("login"))
+
+    return render_template("templates\register.html")
+
+# Home
+@app.route("/home")
+@login_required
+def home():
+    return render_template("templates\home.html", user=current_user.username)
+
+# Logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
